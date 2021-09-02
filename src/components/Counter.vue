b19438f7a2c6f6d276f7317cc01a080fd4206949
Majid Benhenneda <majidbenhenneda@gmail.com>
2021-09-02 15:03:53 +0100

    feat: did a counter in Vue ! :)

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="counter">Counter : {{ counter }}</div>
+  <button @click="increment(counter)">Increment me !</button>
+</template>
+<script>
+export default {
+  name: "Counter",
+  methods: {
+    increment() {
+      this.counter += 1;
+    },
+  },
+  data() {
+    return { counter: 0 };
+  },
+};
+</script>
+<style scoped>
+.counter {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App, {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+}).mount("#app");
